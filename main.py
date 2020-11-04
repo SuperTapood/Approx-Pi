fixed some calculation issues

main.py
@@ -4,8 +4,8 @@ from time import time
 
 
 d = 900
-r = d / 2
-scr = pygame.display.set_mode((d, d))
+r = 450
+scr = pygame.display.set_mode((900, 900))
 pi = 0
 total = 0
 circle = 0
@@ -14,12 +14,13 @@ while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit()
+	pygame.draw.circle(scr, (255, 255, 0), (r, r), 2)
 	pygame.display.update()
-	for i in range(25):
+	for i in range(1250):
 		x, y = np.random.randint(0, d, 2)
 		total += 1
 		color = (255, 0, 0)
-		if np.sqrt(np.square(np.abs(x - r)) + np.square(np.abs(y - r))) < 300:
+		if np.sqrt(np.square(x - r) + np.square(y - r)) <= r:
 			circle += 1
 			color = (255, 255, 0)
 		pygame.draw.circle(scr, color, (x, y), 2)